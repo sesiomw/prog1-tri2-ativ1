38e6335def0f3406a30eb30e8bffea0020ec53e4
Your Name <you@example.com>
2026-05-14 11:36:18 -0300

    Todo List salvando no HD (ainda incompleto)

--- src/core.ts
+++ src/core.ts
@@ -6,16 +6,41 @@ class TodoList {
     private items: Item[] = [];
     private filePath: string;
 
-    constructor(filePath: string){
+    async constructor(filePath: string){
         this.filePath = filePath;
+        await this.readListFromDisk();
     }
 
-    addItem(item: Item){
+    private async saveListToDisk(){
+        const file = Bun.file(this.filePath);
+        const data = JSON.stringify(this.items);
+        await file.write(data);
+    }
+
+    private async readListFromDisk(){
+        const file = Bun.file(this.filePath);
+        // const text = await file.text();
+        // const data = JSON.parse(text);
+        const data = await file.json();
+        this.items = data.map((v: any) => {
+            return new Item(v.title);
+        })
+    }
+
+    /**
+     * Adiciona um item a lista
+     */
+    async addItem(item: Item){
         this.items.push(item);
+        await this.saveListToDisk();
     }
 
-    removeItem(index: number) {
+    /**
+     * Remove um item da lista
+     */
+    async removeItem(index: number) {
         this.items.splice(index, 1);
+        await this.saveListToDisk();
     }
 
     getItems(): Item[] {
