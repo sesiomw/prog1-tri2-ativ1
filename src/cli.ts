c46d6a8154c3bfec820fbac0db265601b5635b75
Your Name <you@example.com>
2026-05-14 11:46:52 -0300

    Iniciando o CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,12 @@
-// CLI: Comand Line Interface
+// CLI: Command Line Interface
+
+const params = process.argv;
+const command = params[2];
+
+if (command === 'add'){
+    const value = params[3];
+    console.log('aqui vai lógica para adicionar um novo item: ${value}');
+    process.exit(0);
+};
+
+console.log('não entrou em nenhum if');
